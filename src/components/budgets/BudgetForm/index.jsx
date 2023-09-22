ae6cd87709f4b400abd124c1ecb85c23933417e6
MiltonBarraza <miltonbarraza90@gmail.com>
2023-09-22 11:22:49 -0300

    cambios realizados

--- src/components/budgets/BudgetForm/index.jsx
+++ src/components/budgets/BudgetForm/index.jsx
@@ -8,13 +8,18 @@ import {
   Form,
   Icon,
   Input,
-  Select,
   Table
 } from 'semantic-ui-react';
 import { ModButton } from "./styles";
 
 const BudgetForm = ({ product }) => {
-  const { register, handleSubmit, control } = useForm();
+  const { register, handleSubmit, control, setValue } = useForm();
+
+  // const { field } = useController({ name: "client", control })
+
+  // const handleSelectChange = (option) => {
+  //   field.onChange(option.value)
+  // }
 
   const onSubmit = (data) => {
     console.log(data);
@@ -24,7 +29,7 @@ const BudgetForm = ({ product }) => {
   const headers = [{
     id: 1, name: ""
   }, {
-    id: 2, name: "Producto"
+    id: 2, name: "Nombre del producto"
   }, {
     id: 3, name: "Precio unitario"
   }, {
@@ -40,10 +45,10 @@ const BudgetForm = ({ product }) => {
   },]
 
   const customers = [
-    { key: '1', value: 'mi', text: 'Milton' },
-    { key: '2', value: 'le', text: 'Levi' },
-    { key: '3', value: 'ga', text: 'Gawain' },
-    { key: '4', value: 'ma', text: 'Marcelo' },
+    { key: '1', value: 'Milton', text: 'Milton' },
+    { key: '2', value: 'Levi', text: 'Levi' },
+    { key: '3', value: 'Gawain', text: 'Gawain' },
+    { key: '4', value: 'Marcelo', text: 'Marcelo' },
   ];
 
   const [products, setProducts] = useState([]);
@@ -56,97 +61,113 @@ const BudgetForm = ({ product }) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Field>
-        <Controller
-          name="customer"
-          control={control}
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Field>
 
-          render={({
-            field: { onChange },
-            fieldState: { invalid, isTouched, isDirty, error },
-            formState,
-          }) =>
-            <Select
-              placeholder="Clientes..."
-              onChange={(data) => "hola"}
-              options={customers} />
-          }
-        />
-        <ModButton
+          {/* <Controller
+            name="client"
+            control={control}
+            render={({ field: { onChange, onBlur, value, ref } }) =>
+              <Form.Select
+                {...field}
+                options={customers}
+                placeholder="Clientes..."
+                onChange={onChange}
+                selected=
+              />}
+          /> */}
+
+          <Form.Select
+            name="client"
+            options={customers}
+            placeholder="Clientes..."
+            onChange={(e, { name, value }) => {
+              setValue(name, value);
+            }}
+          />
+          <ModButton
+            icon
+            labelPosition='right'
+            color="green"
+            type="button"
+            onClick={handleClick}
+          >
+            <Icon name="add" />Agregar producto</ModButton>
+          <Table striped compact celled>
+            <Table.Header>
+              <Table.Row>
+                {headers.map((header) => {
+                  return (
+                    <Table.HeaderCell textAlign="center" key={header.id}>{header.name}</Table.HeaderCell>
+                  )
+                })}
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {products.map(() => {
+                return (
+                  <Table.Row>
+                    <Table.Cell >1</Table.Cell>
+                    <Table.Cell  >
+                      <Controller
+                        name="product.name"
+                        control={control}
+                        render={({ field }) => <Input type="number" fluid transparent {...field} />}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name="product.price"
+                        control={control}
+                        render={({ field }) => <Input type="number" fluid transparent {...field} />}
+                      /></Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name="product.quantity"
+                        control={control}
+                        render={({ field }) => <Input type="number" fluid {...field} />}
+                      /></Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name="product.subtotal"
+                        control={control}
+                        render={({ field }) => <Input type="number" fluid transparent {...field} />}
+                      /></Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name="product.discount"
+                        control={control}
+                        render={({ field }) => <Input type="number" fluid {...field} />}
+                      /></Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name="product.total"
+                        control={control}
+                        render={({ field }) => <Input fluid transparent {...field} />}
+                      /></Table.Cell>
+                    <Table.Cell>
+                      <Link href="/prueba">
+                        <Button color='blue' size="tiny">Editar</Button>
+                      </Link></Table.Cell>
+                  </Table.Row>
+                )
+              })}
+
+            </Table.Body>
+          </Table>
+        </Form.Field>
+        <Button
+          type="submit"
           icon
           labelPosition='right'
           color="green"
-          type="button"
-          onClick={handleClick}
         >
-          <Icon name="add" />Agregar producto</ModButton>
-        <Table striped compact celled>
-          <Table.Header>
-            <Table.Row>
-              {headers.map((header) => {
-                return (
-                  <Table.HeaderCell textAlign="center" key={header.id}>{header.name}</Table.HeaderCell>
-                )
-              })}
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {products.map((product) => {
-              return (
-                <Table.Row>
-                  <Table.Cell >1</Table.Cell>
-                  <Table.Cell  > <Controller
-                    name="name"
-                    control={control}
-                    render={({ field }) => <Input type="text" fluid transparent {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell> <Controller
-                    name="price"
-                    control={control}
-                    render={({ field }) => <Input type="number" fluid transparent {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell> <Controller
-                    name="quantity"
-                    control={control}
-                    render={({ field }) => <Input type="number" fluid {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell> <Controller
-                    name="subtotal"
-                    control={control}
-                    render={({ field }) => <Input type="number" fluid transparent {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell> <Controller
-                    name="discount"
-                    control={control}
-                    render={({ field }) => <Input type="number" fluid {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell> <Controller
-                    name="total"
-                    control={control}
-                    render={({ field }) => <Input fluid transparent {...field} />}
-                  /></Table.Cell>
-                  <Table.Cell>
-                    <Link href="/prueba">
-                      <Button color='blue' size="tiny">Editar</Button>
-                    </Link></Table.Cell>
-                </Table.Row>
-              )
-            })}
-
-          </Table.Body>
-        </Table>
-      </Form.Field>
-      <Button
-        type="submit"
-        icon
-        labelPosition='right'
-        color="green"
-      >
-        <Icon name="add" /> {product?.code ? "Actualizar presupuesto" : "Crear presupuesto"}
-      </Button>
-    </Form>
+          <Icon name="add" /> {product?.code ? "Actualizar presupuesto" : "Crear presupuesto"}
+        </Button>
+      </Form>
+    </>
   )
 };
 
