b71ae0dabeb2fc236f805bb5b68af1c08b9d5f63
MiltonBarraza <miltonbarraza90@gmail.com>
2023-11-30 16:49:55 -0300

    cambios realizados

--- src/components/layout/Footer/Index.jsx
+++ src/components/layout/Footer/Index.jsx
@@ -1,38 +1,37 @@
 "use client";
-// import { PAGES } from "@/constants";
-// import Image from 'next/image';
-// import { usePathname } from 'next/navigation';
-// import { Container, List } from 'semantic-ui-react';
-// import { ModSegment } from "./styles";
+import { PAGES } from "@/constants";
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { Container, List } from 'semantic-ui-react';
+import { ModSegment } from "./styles";
 
 const Footer = () => {
-  // const pathname = usePathname();
+  const pathname = usePathname();
   return (
-    <p>hola</p>
-    // <>
-    //   {pathname !== PAGES.LOGIN.BASE &&
-    //     <ModSegment>
-    //       <Container textAlign='center'>
-    //         <div>
-    //           <Image
-    //             src="/callixto.png"
-    //             alt="Callixto logo"
-    //             width={100}
-    //             height={40}
-    //           />
-    //         </div>
-    //         <List link size='small'>
-    //           <List.Item as='a' href='#'>
-    //             © Copyright 2023 - Todos los derechos reservados por la empresa CallixtoGLM
-    //           </List.Item>
-    //           <List.Item as='a' href='#'>
-    //             Contactanos
-    //           </List.Item>
-    //         </List>
-    //       </Container>
-    //     </ModSegment >
-    //   }
-    // </>
+    <>
+      {pathname !== PAGES.LOGIN.BASE &&
+        <ModSegment>
+          <Container textAlign='center'>
+            <div>
+              <Image
+                src="/callixto.png"
+                alt="Callixto logo"
+                width={100}
+                height={40}
+              />
+            </div>
+            <List link size='small'>
+              <List.Item as='a' href='#'>
+                © Copyright 2023 - Todos los derechos reservados por la empresa CallixtoGLM
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Contactanos
+              </List.Item>
+            </List>
+          </Container>
+        </ModSegment >
+      }
+    </>
   )
 };
 
