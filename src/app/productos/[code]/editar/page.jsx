81bc96cbc32ae3b419cf0ff027ad34b3f75f0486
Nicolas <eb3us2@tutanota.com>
2023-12-01 18:42:19 -0300

    loader crear presupuesto, editar producto

--- src/app/productos/[code]/editar/page.jsx
+++ src/app/productos/[code]/editar/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 import { edit, getProduct } from "@/api/products";
 import ProductForm from "@/components/products/ProductForm";
+import Loader1 from "@/components/layout/Loader";
 import { useEffect, useState } from "react";
 
 const EditProduct = ({ params }) => {
   const [product, setProduct] = useState(null);
+  const [loader, setLoader] = useState(true)
   useEffect(() => {
     const token = localStorage.getItem('token');
     async function fectchData() {
@@ -18,13 +20,18 @@ const EditProduct = ({ params }) => {
       };
       const data = await getProduct(params.code, requestOptions);
       setProduct(data);
+      setLoader(false)
     };
     fectchData();
   }, [params.code]);
 
   return (
     <>
-      {product && <ProductForm product={product} onSubmit={edit} />}
+      {product && 
+        <Loader1 active={loader}>
+          <ProductForm product={product} onSubmit={edit} />
+        </Loader1>
+      }
     </>
   )
 };
