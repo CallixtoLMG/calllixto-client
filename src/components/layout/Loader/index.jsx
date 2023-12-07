d08cc1873130fdc1d91a313bd2663568f730ad3c
Nicolas <eb3us2@tutanota.com>
2023-12-06 22:56:48 -0300

    Loader ordenado

--- src/components/layout/Loader/index.jsx
+++ src/components/layout/Loader/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Dimmer, Loader as LoaderComp, Segment } from "semantic-ui-react";
 
-const Loader = ({children, active}) => {
-    return (
+const Loader = ({ children, active }) => {
+  return (
     <>
-
-  {active ? <LoaderComp active size="large"/> : children }
-        </>
-    );
+      {active ? <LoaderComp active size="large" /> : children}
+    </>
+  );
 };
 
 export default Loader;
