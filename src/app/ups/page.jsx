b2aceae4acd8e2bb878cd85d2fb74d258a88ffc1
Levi Tamiozzo <levi.tzzo@gmail.com>
2023-12-22 13:06:06 -0300

    cambios

--- src/app/ups/page.jsx
+++ src/app/ups/page.jsx
@@ -1,8 +1,23 @@
 "use client"
-import NotFoundPage from "@/components/notFound";
+import { useRouter } from 'next/navigation';
+import { Button, Header, Icon } from 'semantic-ui-react';
+import { MainContainer } from "./styles";
+
 const NotFound = () => {
+  const { back } = useRouter();
+  const handleClick = () => {
+    back();
+    back();
+  };
   return (
-    <NotFoundPage />
+    <MainContainer>
+      <Header as="h1" icon>
+        <Icon color='red' name="exclamation circle" />
+        Página no encontrada
+        o no está autorizado!
+      </Header>
+      <Button onClick={handleClick} color='red'>Volver atrás</Button>
+    </MainContainer>
   );
 };
 
