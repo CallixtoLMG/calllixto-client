a3ebf4aaab0970414d7b3cf1f1fc5c44cc2fc2ca
Levi Tamiozzo <levi.tzzo@gmail.com>
2023-12-25 23:48:49 -0300

    cambios

--- src/components/common/buttons/Edit/styles.jsx
+++
@@ -1,9 +0,0 @@
-import { Icon } from 'semantic-ui-react';
-import styled from 'styled-components';
-
-const ModIcon = styled(Icon)({
-  margin: "0!important",
-});
-
-export { ModIcon };
-
--- src/components/common/buttons/index.jsx
+++ src/components/common/buttons/index.jsx
@@ -1,5 +1,3 @@
-export { default as DeleteButton } from './Delete';
-export { default as EditButton } from './Edit';
 export { default as SendButton } from './Send';
 export { default as DownloadExcelButton } from './DownloadExcel';
 export { default as GoBackButton } from './GoBack';
