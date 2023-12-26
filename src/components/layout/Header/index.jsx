a546338c496fc9e10ab533f7ebc3c5c08dbe057e
MiltonBarraza <miltonbarraza90@gmail.com>
2023-12-26 15:40:24 -0300

    Correciones realizadas

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,26 +1,65 @@
 "use client";
-import { PAGES } from "@/constants";
-import { usePathname } from 'next/navigation';
-import { Menu } from 'semantic-ui-react';
+import { getUserData } from "@/api/userData";
 import { NoPrint } from "@/components/layout";
+import { PAGES } from "@/constants";
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { Menu } from 'semantic-ui-react';
 import { LogDiv, ModContainer, ModLink, Text } from "./styles";
 
 const Header = () => {
+  const [validatedToken, setValidatedToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const pathname = usePathname();
+  const { push } = useRouter();
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    sessionStorage.removeItem("userData");
+    push(PAGES.LOGIN.BASE);
+  };
+  if (typeof window !== 'undefined') {
+    useEffect(() => {
+      const validateToken = async () => {
+        try {
+          const userData = await getUserData();
+          setValidatedToken(userData?.isAuthorized);
+          setIsLoading(false);
+        } catch (error) {
+          setIsLoading(false);
+          console.error('Error, ingreso no valido(token):', error);
+        };
+      };
+      validateToken();
+    }, [localStorage.token]);
+  };
+  const routesWithoutHeader = [PAGES.LOGIN.BASE];
+  const showHeader = !routesWithoutHeader.includes(pathname);
   return (
     <NoPrint>
-      <Menu fixed='top'>
-        <ModContainer>
-          {Object.values(PAGES).filter(page => !!page.NAME).map(page => (
-            <ModLink key={page.BASE} $destacar={pathname.includes(page.BASE)} href={page.BASE}>
-              <Menu.Item><Text $destacar={pathname.includes(page.BASE)}>{page.NAME}</Text></Menu.Item>
-            </ModLink>
-          ))}
-          <LogDiv>
-            <Menu.Item><Text>Cerrar sesión</Text></Menu.Item>
-          </LogDiv>
-        </ModContainer>
-      </Menu>
+      {showHeader &&
+        <Menu fixed='top'>
+          <ModContainer>
+            {isLoading ? (
+              <></>
+            ) : !validatedToken ? (
+              <LogDiv>
+                <Menu.Item onClick={handleLogout} > <Text>Ingresar</Text></Menu.Item>
+              </LogDiv>
+            ) : (
+              <>
+                {Object.values(PAGES).filter(page => !!page.NAME).map(page => (
+                  <ModLink key={page.BASE} $destacar={pathname.includes(page.BASE)} href={page.BASE}>
+                    <Menu.Item><Text $destacar={pathname.includes(page.BASE)}>{page.NAME}</Text></Menu.Item>
+                  </ModLink>
+                ))}
+                <LogDiv>
+                  <Menu.Item onClick={handleLogout} > <Text>Cerrar sesión</Text></Menu.Item>
+                </LogDiv>
+              </>
+            )}
+          </ModContainer>
+        </Menu>
+      }
     </NoPrint>
   );
 };
