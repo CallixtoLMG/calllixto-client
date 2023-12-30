e2828816343db2a603e3b0778ca154404dc65330
Levi Tamiozzo <levi.tzzo@gmail.com>
2023-12-30 14:22:43 -0300

    fix build

--- src/app/productos/crear/page.jsx
+++ src/app/productos/crear/page.jsx
@@ -1,6 +1,8 @@
 "use client";
+import { useListBrands } from "@/api/brands";
 import { create } from "@/api/products";
-import { PageHeader } from "@/components/layout";
+import { useListSuppliers } from "@/api/suppliers";
+import { Loader, PageHeader } from "@/components/layout";
 import ProductForm from "@/components/products/ProductForm";
 import { PAGES } from "@/constants";
 import { useRole, useValidateToken } from "@/hooks/userData";
@@ -10,6 +12,9 @@ const CreateProduct = () => {
   useValidateToken();
   const { push } = useRouter();
   const role = useRole();
+  const { brands, isLoading: isLoadingBrands } = useListBrands();
+  const { suppliers, isLoading: isLoadingSuppliers } = useListSuppliers();
+
   if (role === "user") {
     push(PAGES.NOT_FOUND.BASE)
   };
@@ -17,7 +22,9 @@ const CreateProduct = () => {
   return (
     <>
       <PageHeader title="Crear Producto" />
-      <ProductForm brands={brands} suppliers={suppliers} onSubmit={create} />
+      <Loader active={isLoadingBrands || isLoadingSuppliers}>
+        <ProductForm brands={brands} suppliers={suppliers} onSubmit={create} />
+      </Loader>
     </>
   )
 };
