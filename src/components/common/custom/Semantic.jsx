48a54fa12c6e9c770494c4fd1926782ed67f3902
MiltonBarraza <miltonbarraza90@gmail.com>
2024-02-12 12:42:11 -0300

    cambios realizados

--- src/components/common/custom/Semantic.jsx
+++ src/components/common/custom/Semantic.jsx
@@ -40,10 +40,6 @@ export const Input = styled(SInput)`
   height: ${({ height }) => height || '40px'} !important;
   width: ${({ width }) => width ? width : ''} !important;
   display: flex!important;
-  div.ui.basic.label{
-    padding: 1.23rem 7px!important;
-    align-self: center!important;
-  };
 `;
 
 export const Label = styled(SLabel)`
@@ -63,17 +59,17 @@ export const Dropdown = styled(SDropdown)`
   margin: 5px 0 !important;
   box-shadow: 0 1px 2px 0 rgba(34,36,38,.15)!important;
   border-radius: 0.28571429rem!important;
-  height: ${({ height }) => height || '50px'} !important;
+  height: ${({ height }) => height || '40px'} !important;
   min-height: ${({ minHeight }) => minHeight || 'none'} !important;
   display: flex!important;
   flex-wrap: wrap;
   align-content: center;
   input {
-    height: ${({ height }) => height || '50px'} !important;
+    height: ${({ height }) => height || '40px'} !important;
   };
   i.dropdown.icon {
-    height: 50px!important;
-    top: ${({ top }) => top || "15px"} !important};
+    height: 40px!important;
+    top: ${({ top }) => top || "10px"} !important};
   };
 `;
 
