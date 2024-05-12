5fe8a8c60915a8449ef097844fcd0f4589c44b80
MiltonBarraza <miltonbarraza90@gmail.com>
2024-05-12 16:56:04 -0300

    se borro y elimino las librerias "react-input-mask" y "react-currency-mask"

--- src/components/common/custom/OtherLibraries.jsx
+++ src/components/common/custom/OtherLibraries.jsx
@@ -1,20 +1,5 @@
 import CurrencyFormat from 'react-currency-format';
-import InputMask from 'react-input-mask';
 import styled from "styled-components";
-
-export const MaskedInput = styled(InputMask)`
-  margin: 5px 0 !important;
-  box-shadow: 0 1px 2px 0 rgba(34,36,38,.15)!important;
-  border-radius: 0.28571429rem;
-  height: 50px!important;
-
-  > input {
-      &::-webkit-inner-spin-button,
-      &::-webkit-outer-spin-button {
-        -webkit-appearance: none;
-      };
-  };
-`;
 
 export const CurrencyFormatInput = styled(CurrencyFormat)`
   box-shadow: ${({ shadow }) => shadow && " 0 1px 2px 0 rgba(34,36,38,.15)!important"};
