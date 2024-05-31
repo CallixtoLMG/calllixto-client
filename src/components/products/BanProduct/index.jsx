2936bf0966e3c3848a6a7911d25f4047a32304c9
Levi Tamiozzo <levi.tzzo@gmail.com>
2024-05-30 22:12:00 -0300

    cambiecitos

--- src/components/products/BanProduct/index.jsx
+++ src/components/products/BanProduct/index.jsx
@@ -1,5 +1,5 @@
 import { LIST_BANNED_PRODUCTS_QUERY_KEY, editBanProducts, useListBanProducts } from "@/api/products";
-import { Button, FieldsContainer, Form, FormField, Input, Label, Modal, RuledLabel } from "@/components/common/custom";
+import { Button, FieldsContainer, Form, FormField, Input, Label, Modal } from "@/components/common/custom";
 import { Table } from "@/components/common/table";
 import { Loader } from "@/components/layout";
 import { handleEnterKeyPress } from '@/utils';
@@ -10,20 +10,18 @@ import { toast } from "react-hot-toast";
 import { Transition } from "semantic-ui-react";
 import { BAN_FILTERS, BAN_PRODUCTS_COLUMNS } from "../products.common";
 import { ModalActions } from "./styles";
+import { isEqual, sortBy } from 'lodash';
 
 const BanProduct = ({ open, setOpen }) => {
   const { data: blacklist, isLoading } = useListBanProducts();
-  const { handleSubmit, formState: { isDirty }, setValue, watch } = useForm({ defaultValues: { products: [] } });
+  const { handleSubmit, setValue, watch } = useForm();
   const watchProducts = watch('products');
-  const [errorFlag, setErrorFlag] = useState(false);
-  const [errorFlagMsg, setErrorFlagMsg] = useState("");
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const queryClient = useQueryClient();
+  const [filter, setFilter] = useState('');
 
   const deleteProduct = useCallback((element) => {
     const newProducts = watchProducts.filter(product => product !== element.code);
     setValue("products", newProducts, { shouldDirty: true });
-    setFilteredProducts(newProducts);
   }, [watchProducts, setValue]);
 
   const actions = [
@@ -36,17 +34,18 @@ const BanProduct = ({ open, setOpen }) => {
     }
   ];
 
-  const handleModalClose = () => {
-    setOpen(false);
-  };
-
   const handleAddProduct = (event) => {
     const value = event.target.value;
-    const newCodes = value.split(',').map(code => code.trim().toUpperCase()).filter(code => !!code);
+    const newCodes = value.split(',').map(code => code.trim().toUpperCase()).filter(Boolean);
 
     const error = newCodes.some(code => {
       if (code.includes(' ')) {
         toast.error(`El código [${code}] contiene espacios en blanco, no permitidos!`);
+        return true;
+      }
+
+      if (code.length < 5) {
+        toast.error(`El código [${code}] debe más de 5 caracteres!`);
         return true;
       }
     });
@@ -55,18 +54,14 @@ const BanProduct = ({ open, setOpen }) => {
       return;
     }
 
-    const validProducts = newCodes.filter(code => !watchProducts.includes(code) && !blacklist.includes(code));
-    if (validProducts.length > 0) {
+    const validProducts = newCodes.filter(code => !watchProducts.includes(code));
+    if (validProducts.length) {
       const updatedProducts = [...watchProducts, ...validProducts];
       setValue("products", updatedProducts, { shouldDirty: true });
-      setFilteredProducts(updatedProducts);
-      setErrorFlag(false);
     }
 
     event.target.value = '';
   };
-
-  const onKeyPress = (e) => handleEnterKeyPress(e, handleAddProduct);
 
   const { mutate, isPending } = useMutation({
     mutationFn: async (products) => {
@@ -77,7 +72,7 @@ const BanProduct = ({ open, setOpen }) => {
       if (response.statusOk) {
         queryClient.invalidateQueries({ queryKey: [LIST_BANNED_PRODUCTS_QUERY_KEY] });
         toast.success('Lista de productos bloquedos actualizada!');
-        handleModalClose();
+        setOpen(false);
       } else {
         toast.error(response.message);
       }
@@ -87,34 +82,26 @@ const BanProduct = ({ open, setOpen }) => {
   useEffect(() => {
     if (!isLoading) {
       setValue("products", blacklist);
-      setFilteredProducts(blacklist);
     }
   }, [blacklist, isLoading, setValue]);
-
-  const onFilter = useCallback((filter) => {
-    const filtered = watchProducts.filter(product => product.toLowerCase().includes(filter.code.toLowerCase()));
-    setFilteredProducts(filtered);
-  }, [watchProducts]);
-
-  const hasChanges = filteredProducts.length !== blacklist.length || filteredProducts.some(p => !blacklist.includes(p));
 
   return (
     <Transition animation="fade" duration={500} visible={open}>
       <Modal width="500px"
         closeIcon
         open={open}
-        onClose={handleModalClose}
+        onClose={() => setOpen()}
       >
         <Modal.Content>
           <Form onSubmit={handleSubmit(mutate)}>
             <FieldsContainer>
               <FormField width="100%">
-                <RuledLabel title="Agregar código" message={errorFlag ? errorFlagMsg : ""} />
+                <Label>Agregar código</Label>
                 <Input
                   height="30px"
                   type="text"
                   placeholder="Código"
-                  onKeyPress={onKeyPress}
+                  onKeyPress={(e) => handleEnterKeyPress(e, handleAddProduct)}
                 />
               </FormField>
             </FieldsContainer>
@@ -126,16 +113,18 @@ const BanProduct = ({ open, setOpen }) => {
                   tableHeight="40vh"
                   mainKey="code"
                   headers={BAN_PRODUCTS_COLUMNS}
-                  elements={filteredProducts?.map(p => ({ code: p }))}
+                  elements={watchProducts?.map(p => ({ code: p })).filter(p => p.code.includes(filter))}
                   actions={actions}
                   filters={BAN_FILTERS}
-                  onFilter={onFilter}
-                  onManuallyRestore={() => setFilteredProducts(watchProducts)}
+                  onFilter={(filter) => setFilter(filter.code.toUpperCase().trim())}
+                  onManuallyRestore={() => {
+                    setFilter('');
+                  }}
                 ></Table>
               </Loader >
             </FieldsContainer>
             <ModalActions>
-              <Button disabled={isPending || (!isDirty && !hasChanges)} loading={isPending} type="submit" color="green" content="Aceptar" />
+              <Button disabled={isPending || isEqual(sortBy(blacklist), sortBy(watchProducts))} loading={isPending} type="submit" color="green" content="Aceptar" />
               <Button disabled={isPending} onClick={() => setOpen(false)} color="red" content="Cancelar" />
             </ModalActions>
           </Form>
