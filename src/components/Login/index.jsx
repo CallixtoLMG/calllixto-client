d916137e6486b8b8a80b2cd714808a6248872459
MiltonBarraza <miltonbarraza90@gmail.com>
2024-09-13 00:38:02 -0300

    cambios

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Loader } from "@/components/layout";
-import { PAGES, RULES } from "@/constants";
+import { ICONS, PAGES, RULES } from "@/constants";
 import { useMutation } from "@tanstack/react-query";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
