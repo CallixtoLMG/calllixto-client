f8c12afb17a33cfb76ef0bbafe5ddf64f20353d7
MiltonBarraza <miltonbarraza90@gmail.com>
2024-11-13 19:36:16 -0300

    crear onSuccess redireccionados

--- src/app/clientes/crear/page.jsx
+++ src/app/clientes/crear/page.jsx
@@ -32,6 +32,7 @@ const CreateCustomer = () => {
     },
     onSuccess: async (response) => {
       if (response.statusOk) {
+        push(PAGES.CUSTOMERS.SHOW(response.customer.id))
         toast.success('Cliente creado!');
       } else {
         toast.error(response.error.message);
