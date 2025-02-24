834cfde6ce2f721bb3c9eb24d66e8590a8d81aa3
Levi Tamiozzo <levi.tzzo@gmail.com>
2025-02-23 21:27:16 -0300

    agregar otras formas de pago

--- src/components/budgets/budgets.common.js
+++ src/components/budgets/budgets.common.js
@@ -196,7 +196,9 @@ const PAYMENT_METHODS = [
   { key: 'debito', text: 'Tarjeta de Débito', value: 'Tarjeta de Débito' },
   { key: 'credito', text: 'Tarjeta de Crédito', value: 'Tarjeta de Crédito' },
   { key: 'mercado_pago', text: 'Mercado Pago', value: 'Mercado Pago' },
-  { key: 'dolares', text: 'Dólares', value: 'Dólares' }
+  { key: 'dolares', text: 'Dólares', value: 'Dólares' },
+  { key: 'others', text: 'Otros', value: 'Otros' },
+  { key: 'multi', text: 'Varios', value: 'Varios' }
 ];
 
 const PAYMENT_TABLE_HEADERS = [
