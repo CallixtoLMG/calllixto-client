48f90d12dd7d3d91abbf12d72d9a857ba0a6afc4
Levi Tamiozzo <levi.tzzo@gmail.com>
2025-05-06 12:54:09 -0300

    fix imports

--- src/common/components/custom/index.js
+++ src/common/components/custom/index.js
@@ -1,7 +1,4 @@
-export * from '../form/DatePicker';
-export * from '../form/Password/PasswordField';
 export * from './Containers';
 export * from './Flex';
 export * from './OverflowWrapper';
 export * from './Semantic';
-
