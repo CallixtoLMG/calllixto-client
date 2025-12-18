c2a88d1db8b8f73df9ebcf31db564a2554936b3a
MiltonBarraza <miltonbarraza90@gmail.com>
2025-12-18 11:27:56 -0300

    Correciones realizadas

--- src/app/historial-ventas/page.jsx
+++ src/app/historial-ventas/page.jsx
@@ -2,21 +2,34 @@
 import { useListBudgetsHistory } from "@/api/budgets";
 import BudgetsHistoryFilter from "@/components/budgets/BudgetsHistoryFilters";
 import BudgetsPage from "@/components/budgets/BudgetsPage";
-import { BUDGETS_HISTORY_FILTERS_KEY } from "@/components/budgets/budgets.constants";
+import { BUDGETS_HISTORY_FILTERS_KEY, DATE_RANGE_KEY } from "@/components/budgets/budgets.constants";
 import { useBreadcrumContext, useNavActionsContext } from "@/components/layout";
 import { useValidateToken } from "@/hooks";
-import { useEffect, useMemo, useState } from "react";
+import useFilterParams from "@/hooks/useFilterParams";
+import { useEffect, useMemo } from "react";
 
 const BudgetsHistory = () => {
   useValidateToken();
-  const [dateRange, setDateRange] = useState({ startDate: null, endDate: null });
 
-  const { data, isLoading, refetch, isRefetching } = useListBudgetsHistory(
-    {
-      startDate: dateRange.startDate,
-      endDate: dateRange.endDate,
-    }
-  );
+  const {
+    filters: dateRange,
+    setFilters: setDateRange,
+  } = useFilterParams({
+    key: DATE_RANGE_KEY,
+    defaultParams: {
+      startDate: null,
+      endDate: null,
+    },
+  });
+
+  const {
+    data,
+    isLoading,
+    isRefetching,
+  } = useListBudgetsHistory({
+    startDate: dateRange.startDate,
+    endDate: dateRange.endDate,
+  });
 
   const { setLabels } = useBreadcrumContext();
   const { setActions, setInfo } = useNavActionsContext();
@@ -25,17 +38,11 @@ const BudgetsHistory = () => {
     setLabels([{ name: "Historial de ventas" }]);
   }, [setLabels]);
 
-  useEffect(() => {
-    if (dateRange.startDate || dateRange.endDate) {
-      refetch();
-    }
-  }, [dateRange, refetch]);
-
   const handleSearch = (newRange) => {
     setDateRange(newRange);
   };
 
-  const budgets = useMemo(() => data, [data]);
+  const budgets = useMemo(() => data ?? [], [data]);
   const loading = isLoading || isRefetching;
 
   useEffect(() => {
@@ -45,13 +52,18 @@ const BudgetsHistory = () => {
 
   return (
     <>
-      <BudgetsHistoryFilter onSearch={handleSearch} isLoading={loading} />
+      <BudgetsHistoryFilter
+        onSearch={handleSearch}
+        isLoading={loading}
+        defaultValues={dateRange}
+      />
+
       {dateRange.startDate && dateRange.endDate && (
         <BudgetsPage
           isLoading={loading}
-          budgets={loading ? [] : budgets}
+          budgets={budgets}
           filterKey={BUDGETS_HISTORY_FILTERS_KEY}
-         />
+        />
       )}
     </>
   );
