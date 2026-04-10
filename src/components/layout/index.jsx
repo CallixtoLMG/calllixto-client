30145fc5122a3d4f604a27d9a18ac7fc2a3e38e8
Levi Tamiozzo <levi.tzzo@gmail.com>
2026-04-09 21:44:42 -0300

    remove footer

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,5 +1,5 @@
 export * from "./Breadcrumb";
-export { default as Footer } from "./Footer";
+// export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export * from "./Loader";
 export * from "./NavActions";
