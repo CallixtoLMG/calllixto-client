8bc0ec4283a7f838709b3a9f46d1671ed7934aed
MiltonBarraza <miltonbarraza90@gmail.com>
2026-05-06 16:38:44 -0300

    Boton para imprimir entregas agregado

--- src/components/budgets/BudgetDeliveries/index.jsx
+++ src/components/budgets/BudgetDeliveries/index.jsx
@@ -14,7 +14,7 @@ import { Header } from "../../products/ProductStock/styles";
 import { ADJUST_DELIVERY, DELIVERY, buildBudgetDeliveriesColumns, getDeliveryStats, hasInvalidStockQuantities } from "../budgets.constants";
 import { isBudgetCancelled } from "../budgets.utils";
 
-const BudgetDeliveries = ({ budgetId, onSuccess, state }) => {
+const BudgetDeliveries = ({ budgetId, onSuccess, state, canPrint, onPrint }) => {
   const consumeStock = useConsumeStock();
 
   const { control, setValue } = useFormContext();
@@ -350,6 +350,16 @@ const BudgetDeliveries = ({ budgetId, onSuccess, state }) => {
                 </>
               }
             />
+            {canPrint && (
+              <IconedButton
+                icon={ICONS.PRINT}
+                color={COLORS.BLUE}
+                text="Imprimir entregas"
+                onClick={onPrint}
+                iconOnly
+                popupPosition="top left"
+              />
+            )}
           </Flex>
         </Flex>
         <Table
